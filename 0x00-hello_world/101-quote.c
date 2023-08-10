09f5ce3a530e8b3eb13940a1c8d43ae4a6b59fe1
Nyamisachela <nyamisachelagat@gmail.com>
2023-08-10 05:03:26 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ *main - A program that prints message, new line and standard error
+ *Return: 1 (success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	return (0);
+}
