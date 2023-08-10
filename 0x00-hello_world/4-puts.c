142c21a19a8b78cdccaefffd05ff94280328adf9
Nyamisachela <nyamisachelagat@gmail.com>
2023-08-10 03:48:30 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/*"Programming is like building a multilingual puzzle*/
+
+	return (0);
+}
