b973e219b8f8525cef0ecd078d26113beb479846
Nyamisachela <nyamisachelagat@gmail.com>
2023-08-11 02:59:42 -0700

    This program prints all single digit numbers of base 10 starting from 0, followed by a new line

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,18 +6,11 @@
  */
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
+	int d;
 
-	putchar('\n');
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+		putchar('\n');
 
 	return (0);
 }
