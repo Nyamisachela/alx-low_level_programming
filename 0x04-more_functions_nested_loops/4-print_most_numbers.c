daf5201686adb6f2aa911bb814ac8cb8a7796cd4
Nyamisachela <nyamisachelagat@gmail.com>
2023-08-17 04:37:59 -0700

    prints 0 - 9 but skips 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,13 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = '0' int j = '9'
 
-	for (i = '0', i <= '9', i++)
-	{
-		if (i != '2' && i != '3')
+		while (i <= j)
 		{
-			_putchar(i);
+			if (i != '2' && i != '4')
+				_putchar(i + '0');
+			i++;
 		}
-		_putchar('\n')
-	}
+	_putchar('\n');
 }
