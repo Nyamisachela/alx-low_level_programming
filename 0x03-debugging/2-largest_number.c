42b15e24df4cf2a5d6fc31ef5507e0cdfcd707a9
Nyamisachela <nyamisachelagat@gmail.com>
2023-08-17 10:59:55 -0700

    correctly prints out the largest of three numbers, no matter the case.

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,14 +16,17 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else if (c > a && c > b)
+	else if (a > b && c > a)
 	{
 		largest = c;
 	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
 
 	return (largest);
 }
