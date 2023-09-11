d7a4cf59543f5818b07cdeb3bc16a38dee1ab06f
Nyamisachela <nyamisachelagat@gmail.com>
2023-09-11 02:04:17 -0700

    A header file that defines a macro named PI as an abbreviation for the token 3.14159265359

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,20 @@
+julien@ubuntu:~/0x0c. macro, structures$ cat 1-main.c
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    float a;
+    float r;
+
+    r = 98;
+    a = PI * r * r;
+    printf("%.3f\n", a);
+    return (0);
+}
