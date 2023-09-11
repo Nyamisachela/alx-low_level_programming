db0c5f9d3f4b40c37307e2db4a2491137349008f
Nyamisachela <nyamisachelagat@gmail.com>
2023-09-11 07:27:25 -0700

    new type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a type with theree elements
+ * @name: fisrt element
+ * @age: second element
+ * @owner: third element
+ * Description: long desc
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
